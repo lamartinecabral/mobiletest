fdeb4a4d3f7cbb5181ffd812e296fd9a3f70a9f5
lamartinecabral <11859047+lamartinecabral@users.noreply.github.com>
2020-08-16 05:08:11 -0300

    Update sw.js

--- docs/sw.js
+++ docs/sw.js
@@ -4,10 +4,10 @@ var cacheName = 'v1:static';
 // during the install phase you usually want to cache static assets
 self.addEventListener('install', function(e) {
     // once the SW is installed, go ahead and fetch the resources to make this work offline
-    console.log("install", e);
+    console.log("install", {e});
     e.waitUntil(
         caches.open(cacheName).then(function(cache) {
-            console.log("caches open", cache);
+            console.log("caches open", {cache});
             return cache.addAll([
               './eruda.js',
               './eruda.js.map',
@@ -32,10 +32,10 @@ self.addEventListener('install', function(e) {
 // when the browser fetches a url
 self.addEventListener('fetch', function(event) {
     // either respond with the cached object or go ahead and fetch the actual url
-    console.log("fetch", event);
+    console.log("fetch", {event});
     event.respondWith(
         caches.match(event.request).then(function(response) {
-            console.log("match", response);
+            console.log("match", {response});
             if (response) {
                 // retrieve from cache
                 return response;
